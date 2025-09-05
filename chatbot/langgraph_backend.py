8c6e44718ba8ecae61d7c3606eabd42d2efa0291
Aadarsh4u-code <aadarshkushwaha0208@gmail.com>
2025-09-05 21:02:06 +0100

    sqlite implemented for store checkpoints snapshort

--- chatbot/langgraph_backend.py
+++ chatbot/langgraph_backend.py
@@ -2,10 +2,13 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from langgraph.graph import StateGraph, START, END
 from typing import TypedDict, Annotated
 from langchain_core.messages import BaseMessage, HumanMessage
-from langgraph.checkpoint.memory import InMemorySaver
+from langgraph.checkpoint.sqlite import SqliteSaver
 from langgraph.graph.message import add_messages
-from dotenv import load_dotenv
+import sqlite3
 import os
+
+from dotenv import load_dotenv
+
 
 load_dotenv()
 
@@ -29,8 +32,11 @@ def chat_node(state: ChatState):
     response = llm.invoke(messages)
     return {"messages": [response]}
 
+# Create sqlite database
+conn = sqlite3.connect(database='chatbot.db', check_same_thread=False)
+
 # Checkpointer
-checkpointer = InMemorySaver()
+checkpointer = SqliteSaver(conn=conn)
 
 graph = StateGraph(ChatState)
 graph.add_node("chat_node", chat_node)
@@ -39,5 +45,18 @@ graph.add_edge("chat_node", END)
 
 chatbot = graph.compile(checkpointer=checkpointer)
 
+def retrieve_all_threads():
+    all_threads = set()
+    for checkpoint in checkpointer.list(None):
+        all_threads.add(checkpoint.config['configurable']['thread_id'])
+    return list(all_threads)
 
+# Test
+# CONFIG = {'configurable': {'thread_id':'thread-1'}}
+# response = chatbot.invoke(
+#             {'messages': [HumanMessage(content='what is my name')]}, 
+#             config=CONFIG,
+#         )
+
+# print("response",response)
 
